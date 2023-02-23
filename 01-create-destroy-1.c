8108c3cfc31b352d0c0fd48e1429a569a8c2795f
Matthew Malensek <matt@malensek.net>
2023-02-23 14:22:35 -0800

    Update test case 1

--- 01-create-destroy-1.c
+++ 01-create-destroy-1.c
@@ -19,25 +19,12 @@ test_start("Tests creating and destroying lists. Note that these tests depend on
 
 subtest("Creating and destroying lists",
 {
-    struct elist *list = elist_create(0);
+    struct elist *list = elist_create(0, sizeof(int) * 5);
     test_assert(list != NULL);
     elist_destroy(list);
 
-    list = elist_create(999);
+    list = elist_create(999, sizeof(int) * 100);
     test_assert(list != NULL);
-    elist_destroy(list);
-});
-
-subtest("Checking list size and capacity",
-{
-    struct elist *list = elist_create(0);
-    test_assert(elist_size(list) == 0);
-    test_assert(elist_capacity(list) == 10);
-    elist_destroy(list);
-
-    list = elist_create(999);
-    test_assert(elist_size(list) == 0);
-    test_assert(elist_capacity(list) == 999);
     elist_destroy(list);
 });
 
