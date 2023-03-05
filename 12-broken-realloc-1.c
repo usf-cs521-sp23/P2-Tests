c8979086489c75dc202a8321c73d02200d74e4f4
Matthew Malensek <matt@malensek.net>
2023-03-05 02:06:03 -0800

    Fix broken realloc test

--- 12-broken-realloc-1.c
+++ 12-broken-realloc-1.c
@@ -15,9 +15,10 @@
 #include "elist.h"
 
 struct elist {
-    size_t capacity; /*!< Storage space allocated for list items */
-    size_t size;     /*!< The actual number of items in the list */
-    void **element_storage;
+    size_t capacity;
+    size_t size;
+    size_t item_sz;
+    void *element_storage;
 };
 
 void *realloc(void *ptr, size_t size)
@@ -30,9 +31,10 @@ test_start("Tests functions when realloc() is broken");
 subtest("The list will not be resizable, so go past its capacity. Will likely segfault if realloc errors are not handled correctly.",
 {
     struct elist *list = malloc(sizeof(*list));
-    list->element_storage = malloc(sizeof(void *) * 5);
+    list->element_storage = malloc(sizeof(int) * 5);
     list->capacity = 5;
     list->size = 0;
+    list->item_sz = sizeof(int);
 
     for (int i = 0; i < 100; ++i) {
         elist_add(list, &i);
